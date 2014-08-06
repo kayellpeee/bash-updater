d444471bbf6d8fcf8e2c24c066460bef95e5d10e
Keenan Lidral-Porter <klp2012@gmail.com>
2014-08-06 12:14:19 -0700

    changes commenting and language around setup

--- update.sh
+++ update.sh
@@ -1,39 +1,24 @@
 #! /bin/bash
 
-# This script will pull upstream changes from a specefied repo when provided with the right code.
-# If it fails it will check again in 5 minutes—you can use crontab to set it up to run once a day
-# designed to automate daily pull requests without navigating into the repo and typing git pull
-
-
 # enter the path of the repo you want to update
 repo="/Users/..."
 
 # enter whatever name you save this script file as
 # if you don't put this script in usr/local/bin then make sure to include the file path
-# the variable is used to call itself again on line 32 in case of a failure
+# the variable is used to call itself again on line 28 in case of a failure
 file="update.sh"
 
-# the message you get if git pull fails
+# the message you get if there are no changes yet
 fail="Already up-to-date."
 
-# your passcode
-launchCode=11111
-
 echo "Attempting to update" $repo
-echo -e "please enter proper launch code"
-read -s enteredCode
-if [ $enteredCode -eq $launchCode ]; then 
-	cd "$repo"
-	echo "Checking" $repo "for updates"
-	outpt=$(git pull upstream master)
-	#if fail try again in 5 minutes
-	if [ "$outpt" = "$fail" ]; then
-		echo 'Will try again in 5  minutes'
-		sleep 300 && $file
-	else
-		echo "successfully updated" $repo
-	fi
+cd "$repo"
+echo "Checking" $repo "for updates"
+outpt=$(git pull upstream master)
+#if fail try again in 5 minutes
+if [ "$outpt" = "$fail" ]; then
+	echo 'Will try again in 5  minutes'
+	sleep 300 && $file
 else
-	echo "improper launch code" $enteredCode
+	echo "successfully updated" $repo
 fi
-
